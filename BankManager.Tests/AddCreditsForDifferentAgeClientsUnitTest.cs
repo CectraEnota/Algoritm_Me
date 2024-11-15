b80abc0b0b8a385e6493ba04518b7e3fd3b22b3d
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2024-11-16 00:49:16 +0300

    Refactor tests

--- BankManager.Tests/AddCreditsForDifferentAgeClientsUnitTest.cs
+++ BankManager.Tests/AddCreditsForDifferentAgeClientsUnitTest.cs
@@ -2,13 +2,16 @@
 
 public class AddCreditsForDifferentAgeClientsUnitTest
 {
+    private readonly Bank _sbank = new("СБанк");
+
     [Fact]
     public void CreditForMinorClient()
     {
         var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2010, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\nНичего не найдено.", client.GetCreditHistory());
+        _sbank.IssueCreditTo(client, 1, 100000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + "Ничего не найдено.";
+        Assert.Equal(result, client.GetCreditHistory());
 
     }
 
@@ -16,9 +19,10 @@ public class AddCreditsForDifferentAgeClientsUnitTest
     public void CreditForMinorClientWithBirthdayInThisYear()
     {
         var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2006, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\nНичего не найдено.", client.GetCreditHistory());
+        _sbank.IssueCreditTo(client, 1, 100000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + "Ничего не найдено.";
+        Assert.Equal(result, client.GetCreditHistory());
 
     }
 
@@ -26,9 +30,10 @@ public class AddCreditsForDifferentAgeClientsUnitTest
     public void CreditForClientWithBirthdayInThisYear()
     {
         var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2006, 11, 15), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\n\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _sbank.IssueCreditTo(client, 1, 100000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + $"\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Assert.Equal(result, client.GetCreditHistory());
 
     }
 
@@ -36,9 +41,10 @@ public class AddCreditsForDifferentAgeClientsUnitTest
     public void CreditForPensionerClient()
     {
         var client = new Client("Александрова Александра Александровна", Gender.Woman, new DateTime(1964, 11, 15), [JobType.Teacher], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        Assert.Equal("Кредитная история для Александрова Александра Александровна:\n\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _sbank.IssueCreditTo(client, 1, 100000);
+        var result = "Кредитная история для Александрова Александра Александровна:\n"
+                   + $"\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Assert.Equal(result, client.GetCreditHistory());
 
     }
 
--- BankManager.Tests/AddCreditsToBanksUnitTest.cs
+++ BankManager.Tests/AddCreditsToBanksUnitTest.cs
@@ -2,50 +2,57 @@ namespace bankManager.Tests;
 
 public class AddCreditsToDifferentBanksUnitTest
 {
+    private readonly Client _client = new("Васильев Василий Васильевич", Gender.Man, new DateTime(2004, 12, 24), [JobType.Student], "Нижний Новгород");
+    private readonly Bank _sbank = new("СБанк");
+    private readonly Bank _ibank = new("ЯБанк");
+
     [Fact]
     public void OneCreditInOneBankTest()
     {
-        var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2004, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\n\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _sbank.IssueCreditTo(_client, 1, 100000);
+        var result = $"Кредитная история для Васильев Василий Васильевич:\n"
+                   + $"\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Check(result);
 
     }
 
     [Fact]
     public void TwoCreditsInTwoBanksTest()
     {
-        var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2004, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        var ibank = new Bank("ЯБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        ibank.IssueCreditTo(client, 1, 10000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\n\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до 15.11.2025\n\tкредит в банке ЯБанк под 21% годовых на сумму 10000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _sbank.IssueCreditTo(_client, 1, 100000);
+        _ibank.IssueCreditTo(_client, 1, 10000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + $"\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n"
+                   + $"\tкредит в банке ЯБанк под 21% годовых на сумму 10000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Check(result);
 
     }
 
     [Fact]
     public void TwoCreditsInOneBankTest()
     {
-        var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2004, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        sbank.IssueCreditTo(client, 1, 100000);
-        sbank.IssueCreditTo(client, 1, 10000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\n\tкредит в банке СБанк под 19% годовых на сумму 100000 рублей до 15.11.2025\n\tкредит в банке СБанк под 20% годовых на сумму 10000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _sbank.IssueCreditTo(_client, 10, 100000);
+        _sbank.IssueCreditTo(_client, 1, 10000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + $"\tкредит в банке СБанк под 17% годовых на сумму 100000 рублей до {DateTime.Now.AddYears(10):dd.MM.yyyy}\n"
+                   + $"\tкредит в банке СБанк под 20% годовых на сумму 10000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Check(result);
 
     }
 
     [Fact]
     public void CreditToBadHistoryClient()
     {
-        var client = new Client("Васильев Василий Васильевич", Gender.Man, new DateTime(2004, 12, 24), [JobType.Student], "Нижний Новгород");
-        var sbank = new Bank("СБанк");
-        var ibank = new Bank("ЯБанк");
-        ibank.AddFamousCredit(new Credit(ibank, 17, 100000, new DateTime(2023, 11, 14)), client);
-        sbank.IssueCreditTo(client, 1, 10000);
-        Assert.Equal("Кредитная история для Васильев Василий Васильевич:\n\tкредит в банке ЯБанк под 17% годовых на сумму 100000 рублей до 14.11.2023\n\tкредит в банке СБанк под 22% годовых на сумму 10000 рублей до 15.11.2025\n", client.GetCreditHistory());
+        _ibank.AddFamousCredit(new Credit(_ibank, 17, 100000, new DateTime(2023, 11, 14)), _client);
+        _sbank.IssueCreditTo(_client, 1, 10000);
+        var result = "Кредитная история для Васильев Василий Васильевич:\n"
+                   + $"\tкредит в банке ЯБанк под 17% годовых на сумму 100000 рублей до {new DateTime(2023, 11, 14):dd.MM.yyyy}\n"
+                   + $"\tкредит в банке СБанк под 22% годовых на сумму 10000 рублей до {DateTime.Now.AddYears(1):dd.MM.yyyy}\n";
+        Check(result);
 
     }
+
+    private void Check(string result) => Assert.Equal(result, _client.GetCreditHistory());
 
 }
 
